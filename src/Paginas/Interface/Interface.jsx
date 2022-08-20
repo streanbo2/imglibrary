7bb15a6448e65e25529242a4d2ec6ccb748d630c
streanbo2 <daniel008melo@gmail.com>
2022-08-20 14:01:34 -0400

    feature/02-criaçao-design-iterface

--- src/Paginas/Interface/Interface.jsx
+++ src/Paginas/Interface/Interface.jsx
@@ -1,15 +1,116 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Interface.css'
+import UploadButtons from './UploadButtons';
 
 export default function Interface() {
-    return(
-        <div>
-        <h1>Estamos em Interface</h1>
-        <Link to="/">
-                <button>
+    return (
+        <div className='geral'>
+            <div className='header'>
+                <div class="flex-box container-box">
+                    <div class="content-box">Menu de Imagens</div>
+                </div>
+            </div>
+            <div className='galeria'>
+                <div className='bloco1'>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                </div>
+                <div className='bloco1'>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                    <div className="imagens">
+                        <img src="https://picsum.photos/200" />
+                        <br />
+                        <p class="legend">
+                            Nome da imagem
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='botoes'>
+            <Link to="/">
+                <button className='b1'>
                     SAIR
                 </button>
-            </Link> 
+            </Link>
+            <UploadButtons className='b2'/>
+            </div>
         </div>
     );
 }
